c4e0fcf226a91db5421910bfc26d52d9a30f831f
yashwanth9724 <yashr7409@gmail.com>
2022-02-21 18:03:08 -0600

    update

--- src/main/java/com/javatechie/group/entity/Post.java
+++ src/main/java/com/javatechie/group/entity/Post.java
@@ -22,5 +22,37 @@ public class Post {
     private String userName;
     @Enumerated(EnumType.STRING)
     private PostStatus status;
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public PostStatus getStatus() {
+		return status;
+	}
+	public void setStatus(PostStatus status) {
+		this.status = status;
+	}
+    
+    
 
 }
